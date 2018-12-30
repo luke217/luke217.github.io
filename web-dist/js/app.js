78e2844598c416c7a0b1020b4c073f02dc1250f1
luke217 <lingyu.li@rutgers.edu>
2018-12-31 07:24:48 +0800

    Test PopUp

--- web-dist/js/app.js
+++ web-dist/js/app.js
@@ -44,3 +44,9 @@ $(document).ready(function () {
     console.log('Powered by Jekyll Instagram Portfolio Theme');
     console.log('Project: https://github.com/portfolio-central/jekyll-instagram-portfolio-theme')
 });
+
+$('#videoLink')
+.magnificPopup({
+	  type:'inline',
+	  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  })
